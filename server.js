e000356122d40393162b63345ba37b3f2ef55f1e
David <digital0david@gmail.com>
2019-06-08 12:36:53 -0400

    migration

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const server = express();
 const helmet = require("helmet");
+const cors = require("cors");
 
 const UserRouter = require("./data/user/userRouter.js");
 
 server.use(express.json());
 server.use(helmet());
+server.use(cors());
+
 server.use("/api/users", UserRouter);
 
 module.exports = server;
