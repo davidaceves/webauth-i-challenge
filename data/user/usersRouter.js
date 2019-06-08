e9840303efbc9979bb9148cfebcf2b72d1d4bca9
David <digital0david@gmail.com>
2019-06-08 15:18:09 -0400

    get users, authorized

--- data/user/usersRouter.js
+++ data/user/usersRouter.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const cors = require("cors");
 
 const db = require("../dbConfig.js");
+
+const server = express();
+
+server.use(cors());
 
 router.post("/register", (req, res) => {
   let user = req.body;
@@ -44,6 +49,41 @@ router.post("/login", (req, res) => {
       } else {
         res.status(401).json({ message: "Invalid credentials" });
       }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+function authorize(req, res, next) {
+  const username = req.headers["x-username"];
+  const password = req.headers["x-password"];
+
+  if (!username || !password) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  db("users")
+    .where({ username: username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({
+          message: "Invalid credentials"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+}
+
+router.get("/users", authorize, (req, res) => {
+  db("users")
+    .then(users => {
+      res.status(200).json(users);
     })
     .catch(err => {
       res.status(500).json(err);
