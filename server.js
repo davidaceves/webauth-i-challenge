fb74e182a0d2ec5f6ff2f39a7cbff70368234c8e
David <digital0david@gmail.com>
2019-06-09 22:50:59 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const server = express();
 const helmet = require("helmet");
 const cors = require("cors");
 const session = require("express-session");
@@ -8,8 +7,29 @@ const SessionStore = require("connect-session-knex")(session);
 const AuthRouter = require("./data/auth/authRouter.js");
 const UsersRouter = require("./data/user/usersRouter.js");
 
-server.use(express.json());
+const server = express();
+const sessionConfig = {
+  name: "robot",
+  secret: "secret phrase",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    secure: false,
+    httpOnly: true
+  },
+  store: new SessionStore({
+    knex: require("./data/dbConfig.js"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 60 * 60 * 1000
+  })
+};
+
+server.use(session(sessionConfig));
 server.use(helmet());
+server.use(express.json());
 server.use(cors());
 
 server.use("/api", UsersRouter);
