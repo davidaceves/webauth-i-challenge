381e82e8999b6ab340975b5ae1db283fc548b8c5
David <digital0david@gmail.com>
2019-06-09 22:25:18 -0400

    session dependencies/route

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const server = express();
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
+const SessionStore = require("connect-session-knex")(session);
 
+const AuthRouter = require("./data/auth/authRouter.js");
 const UsersRouter = require("./data/user/usersRouter.js");
 
 server.use(express.json());
@@ -10,5 +13,6 @@ server.use(helmet());
 server.use(cors());
 
 server.use("/api", UsersRouter);
+server.use("/api/auth", AuthRouter);
 
 module.exports = server;
